f92695a6bce3853d8d409dcaf12fddac1efeea30
ko-seunghyun <ccurian86@gmail.com>
2016-01-18 17:52:16 +0900

    카카오 서비스 포기. 동영상 녹화 어플로 전환 service_ui.xml에 토글 버튼 서비스는 바인드로 실행 MediaProjection을 이용하여 Surface를 녹화하는 부분 구현.

--- app/src/main/java/com/tistory/fallingstar/knoti/MainActivity.java
+++ app/src/main/java/com/tistory/fallingstar/knoti/MainActivity.java
@@ -1,17 +1,49 @@
 package com.tistory.fallingstar.knoti;
 
 import android.app.IntentService;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int PERMISSION_CODE = 1;
+    private int mScreenDensity;
+    private MediaProjectionManager mProjectionManager;
+    //private static final int DISPLAY_WIDTH = 480;
+    //private static final int DISPLAY_HEIGHT = 640;
+    private static final int DISPLAY_WIDTH = 1080;
+    private static final int DISPLAY_HEIGHT = 1920;
+    private MediaProjection mMediaProjection;
+    private VirtualDisplay mVirtualDisplay;
+    private MediaProjectionCallback mMediaProjectionCallback;
+    private boolean mServiceOnOff = false;
+    private MediaRecorder mMediaRecorder;
+
+    private MainService mService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +60,21 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        mScreenDensity = metrics.densityDpi;
+
+        mMediaRecorder = new MediaRecorder();
+        initRecorder();
+        prepareRecorder();
+
+        mProjectionManager = (MediaProjectionManager) getSystemService
+                (Context.MEDIA_PROJECTION_SERVICE);
+
+
+
+        mMediaProjectionCallback = new MediaProjectionCallback();
     }
 
     @Override
@@ -52,14 +99,150 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private ServiceConnection mConnection = new ServiceConnection() {
+        // Called when the connection with the service is established
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // Because we have bound to an explicit
+            // service that is running in our own process, we can
+            // cast its IBinder to a concrete class and directly access it.
+            MainService.MainServiceBinder binder = (MainService.MainServiceBinder) service;
+            mService = binder.getService();
+            mService.registerCallback(mCallback);
+        }
+
+        // Called when the connection with the service disconnects unexpectedly
+        public void onServiceDisconnected(ComponentName className) {
+            mService = null;
+        }
+    };
+
+    private MainService.ICallback mCallback = new MainService.ICallback() {
+        public void sendData(boolean flag) {
+
+            mServiceOnOff = flag;
+
+            if(flag == true){
+                shareScreen();
+            }
+            else{
+                mMediaRecorder.stop();
+                mMediaRecorder.reset();
+                Log.v(TAG, "Recording Stopped");
+                stopScreenSharing();
+                initRecorder();
+                prepareRecorder();
+
+                //service stop.
+                Intent Service = new Intent(MainActivity.this, MainService.class);
+                stopService(Service);
+            }
+        }
+    };
+
     public void startServiceMethod(View v){
         Intent Service = new Intent(this, MainService.class);
-        startService(Service);
+        //startService(Service);
+        bindService(Service, mConnection, Context.BIND_AUTO_CREATE);
     }
 
+    /*
     public void endServiceMethod(View v){
         Intent Service = new Intent(this, MainService.class);
         stopService(Service);
+    }*/
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mMediaProjection != null) {
+            mMediaProjection.stop();
+            mMediaProjection = null;
+        }
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != PERMISSION_CODE) {
+            Log.e(TAG, "Unknown request code: " + requestCode);
+            return;
+        }
+        if (resultCode != RESULT_OK) {
+            Toast.makeText(this,
+                    "Screen Cast Permission Denied", Toast.LENGTH_SHORT).show();
+            //mToggleButton.setChecked(false);
+            return;
+        }
+        mMediaProjection = mProjectionManager.getMediaProjection(resultCode, data);
+        mMediaProjection.registerCallback(mMediaProjectionCallback, null);
+        mVirtualDisplay = createVirtualDisplay();
+        mMediaRecorder.start();
+    }
+
+    private void shareScreen() {
+        if (mMediaProjection == null) {
+            startActivityForResult(mProjectionManager.createScreenCaptureIntent(), PERMISSION_CODE);
+            return;
+        }
+        mVirtualDisplay = createVirtualDisplay();
+        mMediaRecorder.start();
+    }
+
+    private void stopScreenSharing() {
+        if (mVirtualDisplay == null) {
+            return;
+        }
+        mVirtualDisplay.release();
+        //mMediaRecorder.release();
+    }
+
+    private VirtualDisplay createVirtualDisplay() {
+        return mMediaProjection.createVirtualDisplay("MainActivity",
+                DISPLAY_WIDTH, DISPLAY_HEIGHT, mScreenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mMediaRecorder.getSurface(), null /*Callbacks*/, null /*Handler*/);
+    }
+
+    private class MediaProjectionCallback extends MediaProjection.Callback {
+        @Override
+        public void onStop() {
+            if (mServiceOnOff) {
+                mServiceOnOff = false;
+                mMediaRecorder.stop();
+                mMediaRecorder.reset();
+                Log.v(TAG, "Recording Stopped");
+                initRecorder();
+                prepareRecorder();
+            }
+            mMediaProjection = null;
+            stopScreenSharing();
+            Log.i(TAG, "MediaProjection Stopped");
+        }
+    }
+
+    private void prepareRecorder() {
+        try {
+            mMediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            finish();
+        } catch (IOException e) {
+            e.printStackTrace();
+            finish();
+        }
+    }
+
+    private void initRecorder() {
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mMediaRecorder.setVideoEncodingBitRate(7776000);
+        mMediaRecorder.setVideoFrameRate(30);
+        mMediaRecorder.setVideoSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+        //mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_1080P));
+        mMediaRecorder.setOutputFile("/sdcard/capture.mp4");
+    }
+
 
 }
--- app/src/main/java/com/tistory/fallingstar/knoti/MainService.java
+++ app/src/main/java/com/tistory/fallingstar/knoti/MainService.java
@@ -4,26 +4,31 @@ import android.accessibilityservice.AccessibilityService;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class MainService extends AccessibilityService {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class MainService extends Service {
 
     private View mView;
     private WindowManager mManager;
+    private ToggleButton mToggleButton;
 
-    /*public MainService() {
-    }*/
-    @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {
-    }
-
-    @Override
-    public void onInterrupt() {
+    public MainService() {
     }
 
     @Override
@@ -38,13 +43,32 @@ public class MainService extends AccessibilityService {
 
                 //WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
                 WindowManager.LayoutParams.TYPE_PHONE,
-
-                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
-
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                //WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
                 PixelFormat.TRANSLUCENT);
 
         mManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         mManager.addView(mView, mParams);
+
+        mToggleButton = (ToggleButton) mView.findViewById(R.id.btn_onOff);
+
+        mToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.btn_onOff:
+                        if(mCallback == null)
+                            return;
+
+                        if (((ToggleButton) v).isChecked()) {
+                            mCallback.sendData(true);
+                        } else {
+                            mCallback.sendData(false);
+                        }
+                        break;
+                }
+            }
+        });
     }
 
     @Override
@@ -59,14 +83,30 @@ public class MainService extends AccessibilityService {
         return super.onStartCommand(intent, flags, startId);
     }
 
-    /*@Override
+    @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }*/
-
-    public void sendMsg(View v){
-        stopSelf();
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
+
+    //local binder create
+    public class MainServiceBinder extends Binder {
+        MainService getService() {
+            return MainService.this;
+        }
+    }
+
+    public interface ICallback {
+        public void sendData(boolean f);
+    }
+
+    private ICallback mCallback;
+
+    public void registerCallback(ICallback cb) {
+        mCallback = cb;
+    }
+
+    private final IBinder mBinder = new MainServiceBinder();
 
 }
